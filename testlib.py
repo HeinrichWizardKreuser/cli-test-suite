e40d1065691891985f835f8d5ba2dd686ba857ec
Heinrich <48674623+HeinrichWizardKreuser@users.noreply.github.com>
2021-05-05 13:53:18 +0200

    Update testlib.py

--- testlib.py
+++ testlib.py
@@ -1,21 +1,189 @@
 import os, re
+import subprocess
 import traceback
+from pathlib import Path
+import fnmatch
 
 QUIET_MODE = False
 
-def _default_is_test_file(filepath):
-    return filepath.endswith('.in')
 
-def _default_in2out(infilepath):
-    if filepath.endswith('.in'):
-        infilepath = infilepath[:len('.in')]
-    return infilepath + '.out'
+
+"""
+def _validate_stderr_func():
+    def f(infilepath, stdout='', stderr=''):
+        return _default_validate_output(infilepath, stdout='', stderr='', stream='stderr'):
+    return f
+"""
+
+def _infilepath2outfilepath(infilepath):
+    # get the test case for this input file
+    outfilepath = infilepath
+    if outfilepath.endswith('.in'):
+        outfilepath = outfilepath[:len('.in')]
+    outfilepath += '.out'
+
+    return outfilepath
+
+class OutputValidators:
+    """
+    default args to OutputValidators:
+        infilpath
+    default kwargs to OutputValidators:
+        stdout
+        stderr
+    """
+    def diff(infilepath, stdout='', stderr='', **kwargs):
+        """ checks difference between  """
+
+        # check for stream
+        stream = kwargs.get('stream')
+        if stream == None:
+            stream = 'stdout'
+
+        # compare their outputs
+        with open(PRODUCED_OUT_FILE, 'w') as temp_file:
+            if stream == 'stdout':
+                temp_file.write(stdout)
+            else:
+                temp_file.write(stderr)
+        
+        # get the test case for this input file
+        outfilepath = _infilepath2outfilepath(infilepath)
+        # now collect the output contents
+        expected_out = file_contents(outfilepath)
+        # write contents to file
+        with open(EXP_OUT_FILE, 'w') as temp_file:
+            temp_file.write(expected_out)
+        
+        # check difference between output
+        diff = os.popen(f"diff {PRODUCED_OUT_FILE} {EXP_OUT_FILE}").read()
+
+        # if there is no difference, we passed
+        if not diff:
+            return '', True
+
+        # else, generate error message
+        failmsg = f'\nRUN:\n\t{kwargs.get("command")}'
+        if ':infilepath:contents}' in EXECUTE:
+            failmsg += f'INPUT:\n{file_contents(infilepath)}\n'
+        failmsg += f'\nOUTPUT:\n---\n{file_contents(PRODUCED_OUT_FILE)}\n---'
+        failmsg += f'\n\nEXPECTED:\n---\n{file_contents(EXP_OUT_FILE)}\n---'
+        return failmsg, False
+        
+
+
+def _execute_test(EXECUTE, infilepath, validate_output):
+
+    # construct command
+    for command in _generate_commands(EXECUTE, infilepath):
+
+        # run program
+        proc = subprocess.Popen(command.split(), 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE, 
+        ) 
+        stdout, stderr = proc.communicate() 
+        stream2output = {
+            'stdout': stdout.decode('utf-8'), 
+            'stderr': stderr.decode('utf-8')
+        }
+
+        msg, passed = validate_output(infilepath, command=command, **stream2output)
+        if passed:
+            yield command
+        else:
+            yield FailedTestException(infilepath, msg)
+
+
+def _generate_commands(EXECUTE, infilepath):
+    """
+    For now we will deprecate the foreach feature until further notice
+    """
+    yield _format_command(EXECUTE, infilepath)
+
+
+def _format_command(EXECUTE, infilepath):
+    if '{args:infilepath}' in EXECUTE:
+        EXECUTE = EXECUTE.replace(
+            '{args:infilepath}', 
+            infilepath)
+
+    if '{pipe:infilepath}' in EXECUTE:
+        EXECUTE = EXECUTE.replace(
+            '{pipe:infilepath}', 
+            f'< {infilepath}')
+
+    if '{args:infilepath:contents}' in EXECUTE:
+        # collect contents
+        infile_contents = file_contents(infilepath)
+        EXECUTE = EXECUTE.replace(
+            '{args:infilepath:contents}', 
+            infile_contents)
+
+    if '{pipe:infilepath:contents}' in EXECUTE:
+        infile_contents = file_contents(infilepath)
+        EXECUTE = EXECUTE.replace(
+            '{pipe:infilepath:contents}', 
+            f'< {infile_contents}')
+
+    return EXECUTE
+
+def _overwrite_test(EXECUTE, infilepath):
+    outfilepath = _infilepath2outfilepath(infilepath)
+
+    # construct command
+    command = _format_command(EXECUTE, infilepath)
+
+    # run program and write to out
+    with open(outfilepath, 'w') as out_file:
+        out_file.write(os.popen(command).read())
+
+WHITE = '\033[37m'
+OK = '\033[32m'
+FAIL = '\033[31m'
+ENDC = '\033[0m'
+UNDERLINE = '\033[4m'
+
+PRODUCED_OUT_FILE = 'temp_in'
+EXP_OUT_FILE = 'temp_out'
+
+
+class FailedTestException(Exception):
+    def __init__(self, infilepath, msg, *args, **kwargs):
+        self.infilepath = infilepath
+        msg = f"{FAIL}FAILED{ENDC} {infilepath}:\n{msg}"
+        super().__init__(msg, *args, **kwargs)
+    def overwrite_msg(self):
+        return f"To overwrite, run:\npython3 test.py overwrite {self.infilepath}\n"
+
+
+def file_contents(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def _create_mode():
+    """
+    Used to create new test cases
+
+    tests are [in] and [out]
+    so the user must specify 
+    - the name of the [in] file (or default to lowest int)
+    - the name of the [out] file (or default to prev name followed by .out)
+    - contents of [in] file
+    - contents of [out] file
+    """
+    pass
+
+# TODO: ifmain
+
+
+
+
 
 
 def main(EXECUTE,
-         testdir='tests/',
-         is_test_file=_default_is_test_file, 
-         in2out=_default_in2out,
+         test_regex='tests/*.in',
+         validate_output=OutputValidators.diff,
          argv=[],
          failfirst=False,
          quiet_mode=False
@@ -58,28 +226,6 @@ def main(EXECUTE,
             contents/is/usually/a/filepath
             ```
             'java MyProgram < contents/is/usually/a/filepath`
-
-
-            5) args:infilepath:contents:foreach
-            'java MyProgram {args:infilepath:contents:foreach}`
-            feeds each line of the contents of each test case filepath as 
-            an argument:
-            ```
-            contents
-            of
-            the
-            testcase
-            ```
-            'java MyProgram contents`
-            'java MyProgram of`
-            'java MyProgram the`
-            'java MyProgram testcase`
-
-            its important to note that the output of each of these will be 
-            compared to each line of the .out file
-
-        TODO
-    
     """
     global QUIET_MODE
     QUIET_MODE = quiet_mode
@@ -100,21 +246,24 @@ def main(EXECUTE,
                 print(f"filapath '{val}' does no exist")
                 return
             _overwrite_test(EXECUTE, val, in2out)
-        # elif flag in ['-c', '--create']:
-        #     break_when_done = True
-        #     _create_mode()
     if break_when_done:
         return
 
-    # collect all of the test filepaths
-    tests = collect_tests(testdir, is_test_file)
+    # collect tests from regex
+    tests = map(
+        str, 
+        filter(
+            lambda x: fnmatch.fnmatch(str(x), test_regex), 
+            Path('.').rglob('*')
+        )
+    )
 
     total = 0
     total_failed = 0
     # parse  execute command
     for testfilepath in tests:
 
-        for outcome in _execute_test(EXECUTE, testfilepath, in2out):
+        for outcome in _execute_test(EXECUTE, testfilepath, validate_output):
             total += 1
             if type(outcome) == FailedTestException:
                 print(outcome)
@@ -142,160 +291,11 @@ def main(EXECUTE,
             if not quiet_mode:
                 print(f"{OK}PASSED{ENDC} {testfilepath}")
 
-    os.remove(f'{PRODUCED_OUT_FILE}')
-    os.remove(f'{EXP_OUT_FILE}')
+    if os.path.isfile(PRODUCED_OUT_FILE):
+        os.remove(PRODUCED_OUT_FILE)
+    if os.path.isfile(EXP_OUT_FILE):
+        os.remove(EXP_OUT_FILE)
     if total_failed > 0:
         print(f"{FAIL}FAILED{ENDC} {total_failed}/{total} tests")
     else:
         print(f"{total}/{total} testcase{'' if total == 1 else 's'} {OK}PASSED{ENDC}")
-
-
-def _execute_test(EXECUTE, infilepath, in2out):
-
-    # check outfile existence
-    outfilepath = in2out(infilepath)
-    if not os.path.exists(outfilepath):
-        yield FailedTestException(infilepath, 
-            f"Could not find outputfile '{outfilepath}'")
-
-    # construct command
-    for command, expected_out in _generate_commands(EXECUTE, infilepath, outfilepath):
-
-        # run program
-        with open(PRODUCED_OUT_FILE, 'w') as temp_file:
-            temp_file.write(os.popen(command).read())
-        
-        with open(EXP_OUT_FILE, 'w') as temp_file:
-            temp_file.write(expected_out)
-        
-        # check difference between output
-        diff = os.popen(f"diff {PRODUCED_OUT_FILE} {EXP_OUT_FILE}").read()
-
-        if diff:
-            failmsg = f'\nRUN:\n\t{command}'
-            if ':infilepath:contents}' in EXECUTE:
-                failmsg += f'INPUT:\n{_file_contents(infilepath)}\n'
-            failmsg += f'\nOUTPUT:\n---\n{_file_contents(PRODUCED_OUT_FILE)}\n---'
-            failmsg += f'\n\nEXPECTED:\n---\n{_file_contents(EXP_OUT_FILE)}\n---'
-            print(diff)
-            yield FailedTestException(infilepath, failmsg)
-        else:
-            yield command
-
-
-def collect_tests(testdir, is_test_file):
-    tests = []
-    for root, dirs, files in os.walk(testdir):
-        for file in files:
-            joined = os.path.join(root, file)
-            if is_test_file(joined):
-                tests.append(joined)
-    return tests
-
-
-def _generate_commands(EXECUTE, infilepath, outfilepath):
-    if '{args:infilepath:contents:foreach}' in EXECUTE:
-        # open the infilepath
-        incontents = _file_contents(infilepath).split('\n')
-        outcontents = _file_contents(outfilepath).split('\n')
-
-        ln = len(incontents)
-
-        if ln != len(outcontents):
-            raise FailedTestException(infilepath, 
-                f"inputfile '{infilepath}' contains {ln} lines while "\
-                f"outputfile '{outfilepath}' contains {len(outcontents)} lines"
-            )
-
-        for i in range(ln):
-            inline, outline = incontents[i], outcontents[i] + '\n'
-            # print(f"inline = |{inline}|")
-            # print(f"outline = |{outline}|")
-
-            if inline == '':
-                return
-
-            yield (
-                EXECUTE.replace('{args:infilepath:contents:foreach}', inline), 
-                outline
-            )
-    else:
-        yield (
-            _format_command(EXECUTE, infilepath), 
-            _file_contents(outfilepath)
-        )
-
-
-def _format_command(EXECUTE, infilepath):
-    if '{args:infilepath}' in EXECUTE:
-        EXECUTE = EXECUTE.replace(
-            '{args:infilepath}', 
-            infilepath)
-
-    if '{pipe:infilepath}' in EXECUTE:
-        EXECUTE = EXECUTE.replace(
-            '{pipe:infilepath}', 
-            f'< {infilepath}')
-
-    if '{args:infilepath:contents}' in EXECUTE:
-        # collect contents
-        infile_contents = _file_contents(infilepath)
-        EXECUTE = EXECUTE.replace(
-            '{args:infilepath:contents}', 
-            infile_contents)
-
-    if '{pipe:infilepath:contents}' in EXECUTE:
-        infile_contents = _file_contents(infilepath)
-        EXECUTE = EXECUTE.replace(
-            '{pipe:infilepath:contents}', 
-            f'< {infile_contents}')
-
-    return EXECUTE
-
-def _overwrite_test(EXECUTE, infilepath, in2out):
-    outfilepath = in2out(infilepath)
-
-    # construct command
-    command = _format_command(EXECUTE, infilepath)
-
-    # run program and write to out
-    with open(outfilepath, 'w') as out_file:
-        out_file.write(os.popen(command).read())
-
-WHITE = '\033[37m'
-OK = '\033[32m'
-FAIL = '\033[31m'
-ENDC = '\033[0m'
-UNDERLINE = '\033[4m'
-
-PRODUCED_OUT_FILE = 'temp_in'
-EXP_OUT_FILE = 'temp_out'
-
-
-class FailedTestException(Exception):
-    def __init__(self, infilepath, msg, *args, **kwargs):
-        self.infilepath = infilepath
-        msg = f"{FAIL}FAILED{ENDC} {infilepath}:\n{msg}"
-        super().__init__(msg, *args, **kwargs)
-    def overwrite_msg(self):
-        return f"To overwrite, run:\nmake overwrite test={self.infilepath}\n"
-
-
-def _file_contents(filepath):
-    with open(filepath) as f:
-        return f.read()
-
-def _create_mode():
-    """
-    Used to create new test cases
-
-    tests are [in] and [out]
-    so the user must specify 
-    - the name of the [in] file (or default to lowest int)
-    - the name of the [out] file (or default to prev name followed by .out)
-    - contents of [in] file
-    - contents of [out] file
-    """
-    pass
-
-# TODO: ifmain
